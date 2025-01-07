09f8fd2982880348e55b2d859e8bf1ac774d8a35
KINGLY <ratanaly92@gmail.com>
2025-01-07 21:29:36 +0700

    update

---
+++ LESSONS/8 GDI/DemoGDI/BasicPaint/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace BasicPaint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbFillColor = new System.Windows.Forms.CheckBox();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            this.btnTriangle = new System.Windows.Forms.Button();
+            this.btnCircle = new System.Windows.Forms.Button();
+            this.btnLine = new System.Windows.Forms.Button();
+            this.btnRectangle = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnPen = new System.Windows.Forms.Button();
+            this.btnColorPicker = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbFillColor
+            // 
+            this.cbFillColor.AutoSize = true;
+            this.cbFillColor.Location = new System.Drawing.Point(392, 40);
+            this.cbFillColor.Name = "cbFillColor";
+            this.cbFillColor.Size = new System.Drawing.Size(65, 17);
+            this.cbFillColor.TabIndex = 14;
+            this.cbFillColor.Text = "Fill Color";
+            this.cbFillColor.UseVisualStyleBackColor = true;
+            // 
+            // trackBar
+            // 
+            this.trackBar.Location = new System.Drawing.Point(98, 25);
+            this.trackBar.Minimum = 1;
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(92, 45);
+            this.trackBar.TabIndex = 13;
+            this.trackBar.Value = 1;
+            // 
+            // btnTriangle
+            // 
+            this.btnTriangle.Location = new System.Drawing.Point(659, 25);
+            this.btnTriangle.Name = "btnTriangle";
+            this.btnTriangle.Size = new System.Drawing.Size(92, 45);
+            this.btnTriangle.TabIndex = 7;
+            this.btnTriangle.Text = "Triangle";
+            this.btnTriangle.UseVisualStyleBackColor = true;
+            this.btnTriangle.Click += new System.EventHandler(this.btnTriangle_Click);
+            // 
+            // btnCircle
+            // 
+            this.btnCircle.Location = new System.Drawing.Point(561, 25);
+            this.btnCircle.Name = "btnCircle";
+            this.btnCircle.Size = new System.Drawing.Size(92, 45);
+            this.btnCircle.TabIndex = 8;
+            this.btnCircle.Text = "Circle";
+            this.btnCircle.UseVisualStyleBackColor = true;
+            this.btnCircle.Click += new System.EventHandler(this.btnCircle_Click);
+            // 
+            // btnLine
+            // 
+            this.btnLine.Location = new System.Drawing.Point(294, 25);
+            this.btnLine.Name = "btnLine";
+            this.btnLine.Size = new System.Drawing.Size(92, 45);
+            this.btnLine.TabIndex = 9;
+            this.btnLine.Text = "Line";
+            this.btnLine.UseVisualStyleBackColor = true;
+            this.btnLine.Click += new System.EventHandler(this.btnLine_Click);
+            // 
+            // btnRectangle
+            // 
+            this.btnRectangle.Location = new System.Drawing.Point(463, 25);
+            this.btnRectangle.Name = "btnRectangle";
+            this.btnRectangle.Size = new System.Drawing.Size(92, 45);
+            this.btnRectangle.TabIndex = 10;
+            this.btnRectangle.Text = "Rectangle";
+            this.btnRectangle.UseVisualStyleBackColor = true;
+            this.btnRectangle.Click += new System.EventHandler(this.btnRectangle_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(865, 288);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 45);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "button2";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btnPen
+            // 
+            this.btnPen.Location = new System.Drawing.Point(196, 25);
+            this.btnPen.Name = "btnPen";
+            this.btnPen.Size = new System.Drawing.Size(92, 45);
+            this.btnPen.TabIndex = 12;
+            this.btnPen.Text = "Pen";
+            this.btnPen.UseVisualStyleBackColor = true;
+            this.btnPen.Click += new System.EventHandler(this.btnPen_Click);
+            // 
+            // btnColorPicker
+            // 
+            this.btnColorPicker.Location = new System.Drawing.Point(3, 3);
+            this.btnColorPicker.Name = "btnColorPicker";
+            this.btnColorPicker.Size = new System.Drawing.Size(92, 90);
+            this.btnColorPicker.TabIndex = 6;
+            this.btnColorPicker.Text = "Color Picker";
+            this.btnColorPicker.UseVisualStyleBackColor = true;
+            this.btnColorPicker.Click += new System.EventHandler(this.btnColorPicker_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(2, 106);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(751, 493);
+            this.pictureBox.TabIndex = 5;
+            this.pictureBox.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.trackBar);
+            this.panel1.Controls.Add(this.btnColorPicker);
+            this.panel1.Controls.Add(this.cbFillColor);
+            this.panel1.Controls.Add(this.btnLine);
+            this.panel1.Controls.Add(this.btnPen);
+            this.panel1.Controls.Add(this.btnTriangle);
+            this.panel1.Controls.Add(this.btnCircle);
+            this.panel1.Controls.Add(this.btnRectangle);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(758, 100);
+            this.panel1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 607);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox cbFillColor;
+        private System.Windows.Forms.TrackBar trackBar;
+        private System.Windows.Forms.Button btnTriangle;
+        private System.Windows.Forms.Button btnCircle;
+        private System.Windows.Forms.Button btnLine;
+        private System.Windows.Forms.Button btnRectangle;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnPen;
+        private System.Windows.Forms.Button btnColorPicker;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ LESSONS/8 GDI/DemoGDI/BasicPaint/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace BasicPaint
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap paintImage;
+        private Graphics paintGraphics;
+        private Point lastPoint;
+        private bool isMouseDown = false;
+        private ColorDialog colorPicker = new ColorDialog();
+        private Bitmap workingImage;
+        private Graphics workingGraphics;
+        private System.Windows.Forms.Button selectedShapeButton;
+        public Form1()
+        {
+            InitializeComponent();
+
+            loadPictureBox();
+            selectedShapeButton = btnPen;
+            selectedShapeButton.BackColor = Color.Red;
+        }
+
+        private void btnColorPicker_Click(object sender, EventArgs e)
+        {
+            colorPicker.ShowDialog();
+        }
+
+        private void btnPen_Click(object sender, EventArgs e)
+        {
+            selectedShapeButton.BackColor = SystemColors.Control;
+            System.Windows.Forms.Button clickedButton = sender as System.Windows.Forms.Button;
+            clickedButton.BackColor = Color.Red;
+            selectedShapeButton = clickedButton;
+        }
+
+        void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = e.Location;
+            isMouseDown = true;
+        }
+
+        void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown)
+            {
+                if (selectedShapeButton.Text == "Pen")
+                {
+                    DrawLineInCanvas(e.Location);
+                }
+                else
+                {
+                    DrawShapeInWorkingImage(e.Location);
+                }
+            }
+        }
+
+        void pictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+            if (selectedShapeButton.Text != "Pen")
+            {
+                DrawShapeInWorkingImage(e.Location);
+                paintImage = new Bitmap(workingImage);
+                paintGraphics = Graphics.FromImage(paintImage);
+                pictureBox.Image = paintImage;
+            }
+        }
+
+        private void DrawShapeInWorkingImage(Point currentPoint)
+        {
+            Pen pen = new Pen(colorPicker.Color, trackBar.Value);
+
+            workingImage = new Bitmap(paintImage);
+            workingGraphics = Graphics.FromImage(workingImage);
+
+            int startPointX = lastPoint.X < currentPoint.X ? lastPoint.X : currentPoint.X;
+            int startPointY = lastPoint.Y < currentPoint.Y ? lastPoint.Y : currentPoint.Y;
+
+            int shapeWidth = (lastPoint.X > currentPoint.X ? lastPoint.X : currentPoint.X) - startPointX;
+            int shapeHeight = (lastPoint.Y > currentPoint.Y ? lastPoint.Y : currentPoint.Y) - startPointY;
+
+            switch (selectedShapeButton.Text)
+            {
+                case "Rectangle":
+                    if (!cbFillColor.Checked)
+                    {
+                        workingGraphics.DrawRectangle(pen, startPointX, startPointY, shapeWidth, shapeHeight);
+                    }
+                    else
+                    {
+                        workingGraphics.FillRectangle(pen.Brush, startPointX, startPointY, shapeWidth, shapeHeight);
+                    }
+                    break;
+
+                case "Circle":
+                    if (!cbFillColor.Checked)
+                    {
+                        workingGraphics.DrawEllipse(pen, startPointX, startPointY, shapeWidth, shapeHeight);
+                    }
+                    else
+                    {
+                        workingGraphics.FillEllipse(pen.Brush, startPointX, startPointY, shapeWidth, shapeHeight);
+                    }
+                    break;
+
+                case "Triangle":
+                    Point point1 = new Point() { X = startPointX, Y = startPointY + shapeHeight };
+                    Point point2 = new Point() { X = startPointX + (shapeWidth / 2), Y = startPointY };
+                    Point point3 = new Point() { X = startPointX + shapeWidth, Y = startPointY + shapeHeight };
+
+                    if (!cbFillColor.Checked)
+                    {
+                        workingGraphics.DrawPolygon(pen, new Point[] { point1, point2, point3 });
+                    }
+                    else
+                    {
+                        workingGraphics.FillPolygon(pen.Brush, new Point[] { point1, point2, point3 });
+                    }
+                    break;
+
+                case "Line":
+                    workingGraphics.DrawLine(pen, lastPoint, currentPoint);
+                    break;
+            }
+
+            if (isMouseDown && selectedShapeButton.Text != "Line")
+            {
+                Pen outLinePen = new Pen(Color.Black);
+                outLinePen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+
+                workingGraphics.DrawRectangle(outLinePen, startPointX, startPointY, shapeWidth, shapeHeight);
+            }
+
+            pictureBox.Image = workingImage;
+        }
+
+        private void DrawLineInCanvas(Point currentPoint)
+        {
+            Pen pen = new Pen(colorPicker.Color, trackBar.Value);
+            paintGraphics.DrawLine(pen, lastPoint, currentPoint);
+            lastPoint = currentPoint;
+            pictureBox.Refresh();
+        }
+
+        private void loadPictureBox()
+        {
+            int width = pictureBox.Width;
+            int height = pictureBox.Height;
+
+            paintImage = new Bitmap(width, height);
+
+            paintGraphics = Graphics.FromImage(paintImage);
+
+            paintGraphics.FillRectangle(Brushes.White, 0, 0, width, height);
+
+            pictureBox.Image = paintImage;
+
+            pictureBox.MouseDown += new MouseEventHandler(pictureBox_MouseDown);
+            pictureBox.MouseMove += new MouseEventHandler(pictureBox_MouseMove);
+            pictureBox.MouseUp += new MouseEventHandler(pictureBox_MouseUp);
+        }
+
+        private void btnLine_Click(object sender, EventArgs e)
+        {
+            selectedShapeButton.BackColor = SystemColors.Control;
+            System.Windows.Forms.Button clickedButton = sender as System.Windows.Forms.Button;
+            clickedButton.BackColor = Color.Red;
+            selectedShapeButton = clickedButton;
+        }
+
+        private void btnRectangle_Click(object sender, EventArgs e)
+        {
+            selectedShapeButton.BackColor = SystemColors.Control;
+            System.Windows.Forms.Button clickedButton = sender as System.Windows.Forms.Button;
+            clickedButton.BackColor = Color.Red;
+            selectedShapeButton = clickedButton;
+        }
+
+        private void btnCircle_Click(object sender, EventArgs e)
+        {
+            selectedShapeButton.BackColor = SystemColors.Control;
+            System.Windows.Forms.Button clickedButton = sender as System.Windows.Forms.Button;
+            clickedButton.BackColor = Color.Red;
+            selectedShapeButton = clickedButton;
+        }
+
+        private void btnTriangle_Click(object sender, EventArgs e)
+        {
+            selectedShapeButton.BackColor = SystemColors.Control;
+            System.Windows.Forms.Button clickedButton = sender as System.Windows.Forms.Button;
+            clickedButton.BackColor = Color.Red;
+            selectedShapeButton = clickedButton;
+        }
+    }
+}
